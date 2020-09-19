4dd94158d8314bba6be976e06dfde030563ba3f1
이준영 <isz01122@naver.com>
2020-09-20 04:03:56 +0900

    final code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//id가 root라는 엘리먼트에 App컴포넌트를 그린다!
+//인접한 JSX요소는 반드시 한개의 태그로 감싸야 한다!!
+//불가능한 코드 예시!
+//ex)ReactDOM.render(<App /><Potato />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ src/routes/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+  return (
+    <div className="about_container">
+      <span>About this page: I built it because I love React.</span>
+      <span>- Dori, 2020</span>
+    </div>
+  );
+}
+
+export default About;
