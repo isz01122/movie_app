e02c4df62d8e46583c7e163f02f5929c8f6482d8
이준영 <isz01122@naver.com>
2020-09-20 04:04:50 +0900

    Updates

---
+++ precache-manifest.c8751fd00d6c5bb7a293769f157c0525.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fb1e722c8d4bfed119a81833476f3a7",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "8668d1b6f57ca714b9c7",
+    "url": "/movie_app/static/css/main.6ec26d2c.chunk.css"
+  },
+  {
+    "revision": "e8e713a700334ff78fe9",
+    "url": "/movie_app/static/js/2.74d8efcf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app/static/js/2.74d8efcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8668d1b6f57ca714b9c7",
+    "url": "/movie_app/static/js/main.5973d836.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
